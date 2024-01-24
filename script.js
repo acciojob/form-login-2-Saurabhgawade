561836c104f17d48edd3b6c246423892f1167d44
Saurabhgawade <72007247+Saurabhgawade@users.noreply.github.com>
2024-01-24 15:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn=document.querySelector("button");
+const frm=document.querySelector("#frm");
+
+btn.addEventListener("click",function(){
+	alert.(frm);
+})
